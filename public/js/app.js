486345919993bb4e830621988abd41a2bab97e76
Ikechukwu Orji <ikeuzo57@gmail.com>
2016-08-14 18:26:03 -0500

    add join chat screen Query Params

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,23 @@
 
 var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 
+console.log( name + ' ' + ' wants to join '+ ' ' + room);
 socket.on('connect', function(){
 		console.log("Connected to socket.io Server!!");
 });
 
 socket.on('message', function(message){
 	var momentTimestamp = moment.utc(message.timestamp); 
+	var $message = jQuery('.messages');
+
 	console.log('New message');
 	console.log(message.text);
-
-	jQuery('.messages').append('<p><strong>' +  momentTimestamp.local().format('h:mm a') + '</strong>: ' + ' '  + message.text + '</p>')
+	$message.append('<p><strong>' + message.name + ' '+  momentTimestamp.local().format('h:mm a') + '</strong></p>')
+	$message.append('<p>' + message.text + '</p>');
 });
+
 
 // Handles submitting of new message
 
@@ -23,6 +29,7 @@ $form.on('submit', function(event){
 
 	var $message = $form.find('input[name=message]');
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 	
