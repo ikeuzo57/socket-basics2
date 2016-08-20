10c32e6ff899a8ecb67a5ec7fa2c328607e0fec3
Ikechukwu Orji <ikeuzo57@gmail.com>
2016-08-20 16:19:43 -0500

    Add disconnect event and message

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ var clientInfo = {};
 io.on('connection', function(socket){
 	console.log('User Connected via socket.io!!');
 
+	socket.on('disconnect', function(){
+		var userData = clientInfo[socket.id];
+		
+		if(typeof userData !== 'undefined'){
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left!',
+				timestamp: moment().valueOf()
+			})
+			delete userData; 
+
+		}
+	});
+
+	// enables private room chat on server
 	socket.on('joinRoom', function(req){
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
